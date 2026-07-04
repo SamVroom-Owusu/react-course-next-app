8b0be50e5aef3b0e1384a6a043484b11f7d051b8
Sam V-O <sam.v.owusu@gmail.com>
2026-07-04 16:02:01 +0100

    created meal item and grid components

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -3,7 +3,7 @@
 export default function MealsDetailsPage({ params }) {
     return (
         <div>
-            <h1>New Post</h1>
+            <h1>Meal Details</h1>
             <p>{ params.slug }</p>
         </div>
     )
