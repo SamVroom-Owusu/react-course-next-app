71317a26bbb022b1c80cd974d1c515137929a82f
Sam V-O <sam.v.owusu@gmail.com>
2026-07-05 19:32:00 +0100

    working with server action responses

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,10 +1,16 @@
+'use client';
+
+import { useActionState } from 'react'; // react-dom useFormState has been renamed to useActionState
+
 import ImagePicker from '@/components/meals/image-picker';
 import classes from './page.module.css';
-
-import { ShareMeal } from '@/lib/actions';
+import { shareMeal } from '@/lib/actions';
 import MealsFormSubmit from '@/components/meals/meals-form-submit';
 
 export default function ShareMealPage() {
+  // TODO: fix formAction validation trim not working as expected
+  const [state, formAction] = useActionState(shareMeal, { message: null });
+
   return (
     <>
       <header className={classes.header}>
@@ -14,7 +20,7 @@ export default function ShareMealPage() {
         <p>Or any other meal you feel needs sharing!</p>
       </header>
       <main className={classes.main}>
-        <form className={classes.form} action={ShareMeal}>
+        <form className={classes.form} action={formAction}>
           <div className={classes.row}>
             <p>
               <label htmlFor="name">Your name</label>
@@ -43,6 +49,7 @@ export default function ShareMealPage() {
             ></textarea>
           </p>
           <ImagePicker label="Your image" name="image" />
+          {state.message && <p>{state.message}</p>}
           <p className={classes.actions}>
             <MealsFormSubmit />
           </p>
