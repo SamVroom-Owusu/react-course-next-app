5f23ab03d2933f3139ccac2cf749f7027951335b
Sam V-O <sam.v.owusu@gmail.com>
2026-07-05 13:28:41 +0100

    update the share page and create image picker

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,11 +1,50 @@
-import Link from "next/link";
+import ImagePicker from '@/components/meals/image-picker';
+import classes from './page.module.css';
 
-export default function ShareMealpage({ params }) {
-    return (
-        <div>
-            <h1>Share Your Meal</h1>
-            <p>Here you can share your favorite meal with the community.</p>
-            <p><Link href="/meals">Back to Meals</Link></p>
-        </div>
-    )
+export default function ShareMealPage() {
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>
+          Share your <span className={classes.highlight}>favorite meal</span>
+        </h1>
+        <p>Or any other meal you feel needs sharing!</p>
+      </header>
+      <main className={classes.main}>
+        <form className={classes.form}>
+          <div className={classes.row}>
+            <p>
+              <label htmlFor="name">Your name</label>
+              <input type="text" id="name" name="name" required />
+            </p>
+            <p>
+              <label htmlFor="email">Your email</label>
+              <input type="email" id="email" name="email" required />
+            </p>
+          </div>
+          <p>
+            <label htmlFor="title">Title</label>
+            <input type="text" id="title" name="title" required />
+          </p>
+          <p>
+            <label htmlFor="summary">Short Summary</label>
+            <input type="text" id="summary" name="summary" required />
+          </p>
+          <p>
+            <label htmlFor="instructions">Instructions</label>
+            <textarea
+              id="instructions"
+              name="instructions"
+              rows="10"
+              required
+            ></textarea>
+          </p>
+          <ImagePicker label="Pick an image" name="image" />
+          <p className={classes.actions}>
+            <button type="submit">Share Meal</button>
+          </p>
+        </form>
+      </main>
+    </>
+  );
 }
