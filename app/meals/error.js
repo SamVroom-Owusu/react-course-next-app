c57642e1729ed6411487ae78bad9ae54f29d0fae
Sam V-O <sam.v.owusu@gmail.com>
2026-07-05 11:58:49 +0100

    loading, not-found and error pages

---
+++ app/meals/error.js
@@ -0,0 +1,11 @@
+"use client";
+
+export default function Error() {
+    return (
+        <main className="error">
+            <h1>Something went wrong!</h1>
+            <p>We are sorry, but we could not load the meals.</p>
+        </main>
+    )
+
+}
